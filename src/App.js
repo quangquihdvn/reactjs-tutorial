c3b44802deb0e663eb571d7ae6319a5bc6cf07c6
quangquihdvn1 <quangquihdvn@gmail.com>
2024-03-19 00:03:40 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -47,8 +47,18 @@ export default function Board() {
     return null;
   }
 
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
